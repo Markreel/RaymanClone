5f81dd92bc8a7c2d6617579b74b954049b265e32
Markreel <markmeerssman@live.nl>
2020-07-01 00:29:38 +0200

    Finished visuals for superpower pickup model Started working on superpower duration UI

--- Rayman Clone/Assets/Script/SuperPowerCan.cs
+++ Rayman Clone/Assets/Script/SuperPowerCan.cs
@@ -3,6 +3,7 @@
 public class SuperPowerCan : MonoBehaviour
 {
     [SerializeField] public SuperPower SuperPower;
+    [SerializeField] private GameObject beam;
     private Vector3 startPos;
 
     private void Awake()
@@ -13,5 +14,17 @@ public class SuperPowerCan : MonoBehaviour
     private void Update()
     {
         transform.position = startPos + new Vector3(0f,Mathf.Cos(Time.time * 2)/20,0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rayman _rayman = other.GetComponent<Rayman>();
+        if(_rayman != null && !beam.activeInHierarchy) { beam.SetActive(true); }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Rayman _rayman = other.GetComponent<Rayman>();
+        if (_rayman != null && beam.activeInHierarchy) { beam.SetActive(false); }
     }
 }
